57b6cd92df34a86d030a138e3888b58cb2dd03e3
taoistwar <taoistwar@gmail.com>
2026-06-05 10:34:34 +0800

    删除 get_proxy

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,13 +1,11 @@
 pub mod app_state;
 pub mod health;
 pub mod post_proxy;
-pub mod get_proxy;
 pub mod server;
 pub use app_state::*;
 pub use health::*;
 pub use post_proxy::*;
 pub use server::*;
-pub use get_proxy::*;
 
 pub fn request_is_streaming(body_bytes: &[u8]) -> bool {
     serde_json::from_slice::<serde_json::Value>(body_bytes)
